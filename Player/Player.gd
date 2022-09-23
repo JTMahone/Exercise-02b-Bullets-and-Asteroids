83dbcf0f5031bfacc93d7776d96ba907ff46fb90
JTMahone <92094508+JTMahone@users.noreply.github.com>
2022-09-23 12:25:12 -0400

    2b exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,19 +2,20 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
-var rotation_speed = 5.0
-var speed = 0.1
+var rotation_speed = 4
+var speed = 2
 var max_speed = 10
-
-
+var nose = Vector2(0,-60)
+onready var Bullet = load("res://Bullet.tscn")
 
 func _ready():
 	pass
-
 func _physics_process(_delta):
 	position = position + velocity
 	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
-	
+	position.x = wrapf(position.x, 0, 1024)
+	position.y = wrapf(position.y, 0, 600)
+
 	$Exhaust.hide()
 	if Input.is_action_pressed("forward"):
 		velocity = velocity + Vector2(0,-speed).rotated(rotation)
@@ -23,6 +24,10 @@ func _physics_process(_delta):
 		rotation_degrees = rotation_degrees - rotation_speed
 	if Input.is_action_pressed("right"):
 		rotation_degrees = rotation_degrees + rotation_speed
-		
-	position.x = wrapf(position.x, 0, 1024)
-	position.y = wrapf(position.y, 0, 600)
+	if Input.is_action_just_pressed("shoot"):
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var bullet = Bullet.instance()
+			bullet.global_position = global_position + nose.rotated(rotation)
+			bullet.rotation = rotation
+			Effects.add_child(bullet)
